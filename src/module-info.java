799172aadf9711312d87732c6f4a448dc5e12067
unknown <torresgabriel70537@gmail.com>
2024-06-25 21:29:42 -0500

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module views {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens views to javafx.fxml;
+//    opens controllers to javafx.fxml;
+//    opens objects to javafx.fxml;
+    exports views;
+//    exports controllers;
+//    exports objects;
+}
---
+++ src/views/main.java
@@ -0,0 +1,35 @@
+package views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+            // Cargar el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("prototipo2.fxml"));
+            Parent root = loader.load();
+            // Crear la escena con el contenido cargado
+            Scene scene = new Scene(root);
+            // Configurar el escenario (stage)
+            primaryStage.setTitle("Bienvenido");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	
+}
