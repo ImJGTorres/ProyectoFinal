6d13d25f852c064bfd871b6385b1cec968e52da8
unknown <torresgabriel70537@gmail.com>
2024-06-27 17:52:03 -0500

    actualizando controller

--- src/controllers/pantallaController.java
+++ src/controllers/pantallaController.java
@@ -1,12 +1,11 @@
 package controllers;
-/**
- * Sample Skeleton for 'pantalla.fxml' Controller Class
- */
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,11 +13,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
@@ -47,9 +44,6 @@ public class pantallaController {
     
     @FXML // fx:id="lvChatGrupal"
     private ListView<String> lvChatGrupal; // Value injected by FXMLLoader
-
-    @FXML // fx:id="lvChatPrivado"
-    private ListView<String> lvChatPrivado; // Value injected by FXMLLoader
 
     @FXML // fx:id="tabPaneChats"
     private TabPane tabPaneChats; // Value injected by FXMLLoader
@@ -82,11 +76,16 @@ public class pantallaController {
     private TextField txtUsuarioPrivado; // Value injected by FXMLLoader
     
     private ObservableList<String> listaMensajesGrupales;
-    private ObservableList<String> listaMensajesPrivados;
+    private ArrayList<String> usuariosActivos;
+    private ArrayList<String> usuariosInactivos;
+    private Map<Tab, ObservableList<String>> mensajesPrivadosPorTab;
+
     
     public pantallaController() {
     	listaMensajesGrupales = FXCollections.observableArrayList();
-    	listaMensajesPrivados = FXCollections.observableArrayList();
+    	mensajesPrivadosPorTab = new HashMap<>();
+    	usuariosActivos = new ArrayList<String>();
+    	usuariosInactivos = new ArrayList<String>();
     }
 
     @FXML
@@ -95,71 +94,138 @@ public class pantallaController {
     	tabChatPrivado.setDisable(true);
     	tabChatGrupal.setDisable(true);
     	tabPanePrincipal.getSelectionModel().select(tabBienvenida);   
+    	usuariosActivos.remove(txtUsuarioPrivado.getText());
+    	usuariosInactivos.add(txtUsuarioPrivado.getText());
+    	cerrarTabs(tabPaneChats);
     }
-
+    
+    void cerrarTabs(TabPane panel) {
+    	tabPaneChats.getTabs().clear();
+        mensajesPrivadosPorTab.clear();
+    }
     @FXML
     void enviarMensajeGrupal(ActionEvent event) {
+    	listaMensajesGrupales.add(txtUsuarioGrupal.getText() + obtenerHora() + ": " + txtMensajeGrupal.getText());
+    	txtMensajeGrupal.clear();
+    }
+    
+    String obtenerHora() {
     	Calendar c = Calendar.getInstance();
-//    	int dia = c.get(Calendar.DAY_OF_MONTH);
-//    	int mes = c.get(Calendar.MONTH)+1;
-//    	int anio = c.get(Calendar.YEAR);
     	int hora = c.get(Calendar.HOUR_OF_DAY);
     	int minuto = c.get(Calendar.MINUTE);
-        
     	String fechaHora = "(" + hora + ":" + minuto + ")";
-    	listaMensajesGrupales.add(txtUsuarioGrupal.getText() + fechaHora + ": " + txtMensajeGrupal.getText());
+    	return fechaHora;
     }
 
     @FXML
     void enviarMensajePrivado(ActionEvent event) {
-    	Calendar c = Calendar.getInstance();
-    	int hora = c.get(Calendar.HOUR_OF_DAY);
-    	int minuto = c.get(Calendar.MINUTE);
+    	Tab tabActual = tabPaneChats.getSelectionModel().getSelectedItem(); 
+    	AnchorPane panel = (AnchorPane)tabActual.getContent();
+    	
+    	ListView<String> chatPrivado = null;
+        for (javafx.scene.Node node : panel.getChildren()) {
+            if (node instanceof ListView) {
+                chatPrivado = (ListView<String>) node;
+                break;
+            }
+        }
         
-    	String fechaHora = "(" + hora + ":" + minuto + ")";
-    	listaMensajesPrivados.add(txtUsuarioPrivado.getText() + fechaHora + ": " + txtMensajePrivado.getText());
+        //obtener mensajes para esa pestaña
+        ObservableList<String> listaMensajesPrivados = mensajesPrivadosPorTab.get(tabActual);
+        if (!txtMensajePrivado.getText().equals("")) {
+        	listaMensajesPrivados.add(txtUsuarioPrivado.getText() + obtenerHora() + ": " + txtMensajePrivado.getText());
+            chatPrivado.setItems(listaMensajesPrivados); 
+            txtMensajePrivado.clear();
+        } else {
+        	alertaMensajeVacio();
+        }
+        
     }
     
     @FXML
     void irAChatPrivado(ActionEvent event) {
     	if (txtIngresarUsuario.getText().equals("")) {
-    		Alert alert = new Alert(Alert.AlertType.WARNING, "Ingrese un nombre de usuario valido", ButtonType.CLOSE);
-            alert.setTitle("Nombre de usuario invalido");
-            alert.showAndWait();
-    	} else {
-    		txtUsuarioGrupal.setText(txtIngresarUsuario.getText());
-    		txtUsuarioPrivado.setText(txtIngresarUsuario.getText());
-        	tabBienvenida.setDisable(true);
-        	tabChatPrivado.setDisable(false);
-        	tabChatGrupal.setDisable(false);
+    		alertaUsuarioInvalido();
+    	} else if (añadirUsuario(txtIngresarUsuario.getText())) {
+    		irAChat();
         	tabPanePrincipal.getSelectionModel().select(tabChatPrivado);        
     	}
-    }
-    
-    void agregarChats() {
-    	Tab nueva = new Tab("Chat con " + txtIngresarUsuario.getText());
-    	AnchorPane contenido = new AnchorPane();
-    	TextArea txtArea = new TextArea();
-    	txtArea.setPrefSize(660, 250);
-    	contenido.getChildren().add(txtArea);
-    	nueva.setContent(contenido);
-    	this.tabPaneChats.getTabs().add(nueva);
     }
 
     @FXML
     void irAChatGrupal(ActionEvent event) {
     	if (txtIngresarUsuario.getText().equals("")) {
-    		Alert alert = new Alert(Alert.AlertType.WARNING, "Ingrese un nombre de usuario valido", ButtonType.CLOSE);
-            alert.setTitle("Nombre de usuario invalido");
-            alert.showAndWait();
-    	} else {
-    		txtUsuarioGrupal.setText(txtIngresarUsuario.getText());
-    		txtUsuarioPrivado.setText(txtIngresarUsuario.getText());
-        	tabBienvenida.setDisable(true);
-        	tabChatPrivado.setDisable(false);
-        	tabChatGrupal.setDisable(false);    
+    		alertaUsuarioInvalido();
+    	} else if (añadirUsuario(txtIngresarUsuario.getText())) {
+    		irAChat();
             tabPanePrincipal.getSelectionModel().select(tabChatGrupal);  
+    	} 
+    }
+    
+    void irAChat() {
+    	crearChatsPrivados();
+		txtUsuarioGrupal.setText(txtIngresarUsuario.getText());
+		txtUsuarioPrivado.setText(txtIngresarUsuario.getText());
+    	tabBienvenida.setDisable(true);
+    	tabChatPrivado.setDisable(false);
+    	tabChatGrupal.setDisable(false);    
+    }
+
+    void crearChatsPrivados() {
+    	for(String usuario: usuariosActivos) {
+    		if (!usuario.equals(txtIngresarUsuario.getText())) {
+    			agregarChats(usuario);
+    		}
     	}
+    }
+    
+    void agregarChats(String usuario) {
+    	Tab nueva = new Tab("Chat con " + usuario);
+    	AnchorPane contenido = new AnchorPane();
+    	ListView<String> lv = new ListView<>();
+    	lv.setId(usuario);
+    	lv.setPrefSize(660, 250);
+    	contenido.getChildren().add(lv);
+    	nueva.setContent(contenido);
+    	this.tabPaneChats.getTabs().add(nueva);
+    
+        ObservableList<String> listaMensajesPrivados = FXCollections.observableArrayList();
+        mensajesPrivadosPorTab.put(nueva, listaMensajesPrivados);
+    }
+    
+    boolean añadirUsuario(String usuario) {
+    	boolean puedeAgregar = true;
+    	if (usuariosActivos.contains(usuario)) puedeAgregar = false;
+    		
+    	for(String s: usuariosInactivos) {
+    		if (s.equals(usuario) && puedeAgregar) {
+    			usuariosInactivos.remove(usuario);
+    			break;
+    		}
+    	}
+    	
+    	if (puedeAgregar) usuariosActivos.add(usuario);
+    	else alertaErrorInicioSesion();
+    	
+    	return puedeAgregar;
+    }
+    
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String contenido) {
+        Alert alert = new Alert(tipo, contenido, ButtonType.CLOSE);
+        alert.setTitle(titulo);
+        alert.showAndWait();
+    }
+    
+    void alertaUsuarioInvalido() {
+    	mostrarAlerta(Alert.AlertType.WARNING, "Nombre de usuario inválido", "Ingrese un nombre de usuario válido.");
+    }
+    
+    void alertaErrorInicioSesion() {
+    	mostrarAlerta(Alert.AlertType.WARNING, "Error de inicio de sesión", "El usuario ya está en el sistema.");
+    }
+    
+    void alertaMensajeVacio() {
+    	mostrarAlerta(Alert.AlertType.WARNING, "Error de mensaje", "El mensaje que intenta enviar está vacío.");
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -170,7 +236,6 @@ public class pantallaController {
         assert cmdEnviarMensajeGrupal != null : "fx:id=\"cmdEnviarMensajeGrupal\" was not injected: check your FXML file 'pantalla.fxml'.";
         assert cmdEnviarMensajePrivado != null : "fx:id=\"cmdEnviarMensajePrivado\" was not injected: check your FXML file 'pantalla.fxml'.";
         assert lvChatGrupal != null : "fx:id=\"lvChatGrupal\" was not injected: check your FXML file 'pantalla.fxml'.";
-        assert lvChatPrivado != null : "fx:id=\"lvChatPrivado\" was not injected: check your FXML file 'pantalla.fxml'.";
         assert tabBienvenida != null : "fx:id=\"tabBienvenida\" was not injected: check your FXML file 'pantalla.fxml'.";
         assert tabChatGrupal != null : "fx:id=\"tabChatGrupal\" was not injected: check your FXML file 'pantalla.fxml'.";
         assert tabChatPrivado != null : "fx:id=\"tabChatPrivado\" was not injected: check your FXML file 'pantalla.fxml'.";
@@ -185,7 +250,6 @@ public class pantallaController {
         tabChatGrupal.setDisable(true);
         tabChatPrivado.setDisable(true);
         lvChatGrupal.setItems(listaMensajesGrupales);
-        lvChatPrivado.setItems(listaMensajesPrivados);
     }
 
 }
